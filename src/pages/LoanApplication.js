be01cb768bede88879aeb011a48a390ea6ee00f2
kipngenohaaron <kipngenohaaron@gmail.com>
2024-12-06 15:46:31 +0300

    loanapplication.js modified

---
+++ src/pages/LoanApplication.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoanApplication = () => (
+  <div className="container mt-5">
+    <h2>Loan Application</h2>
+    <p>Fill out the form below to apply for a loan.</p>
+    {/* Placeholder for loan application form */}
+    <p>Loan form coming soon!</p>
+  </div>
+);
+
+export default LoanApplication;
